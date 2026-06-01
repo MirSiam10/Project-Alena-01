723a4eaebe3f49bdd801e1c5478f88b2df1af899
MirSiam10 <mir10siam@gmail.com>
2026-06-02 00:50:17 +0600

    Alena's Birth

---
+++ main.py
@@ -0,0 +1,122 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import musicLibrary
+import requests
+from openai import OpenAI
+from gtts import gTTS
+import pygame
+import os
+
+# pip install pocketsphinx
+# for index, name in enumerate(sr.Microphone.list_microphone_names()):
+#     print(f"{index}: {name}")
+import speech_recognition as sr
+
+# print(sr.Microphone.list_microphone_names())
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+# Set your NewsAPI key here
+newsapi = "Your NewsAPI Key"
+
+def speak_old(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def speak(text):
+    tts = gTTS(text)
+    tts.save('temp.mp3') 
+
+    # Initialize Pygame mixer
+    pygame.mixer.init()
+
+    # Load the MP3 file
+    pygame.mixer.music.load('temp.mp3')
+
+    # Play the MP3 file
+    pygame.mixer.music.play()
+
+    # Keep the program running until the music stops playing
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+    
+    pygame.mixer.music.unload()
+    os.remove("temp.mp3") 
+
+def aiProcess(command):
+    client = OpenAI(api_key="<Your OpenAI API Key>",
+    )
+
+    completion = client.chat.completions.create(
+    model="gpt-5.5 Instant",
+    messages=[
+        {"role": "system", "content": "You are a virtual assistant named Alena skilled in general tasks like Alexa and Google Cloud. Give short responses please"},
+        {"role": "user", "content": command}
+    ]
+    )
+
+    return completion.choices[0].message.content
+
+def processCommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://facebook.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+    elif "open linkedin" in c.lower():
+        webbrowser.open("https://linkedin.com")
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        link = musicLibrary.music[song]
+        webbrowser.open(link)
+
+    elif "news" in c.lower():
+        r = requests.get(f"https://newsapi.org/v2/top-headlines?country=us&apiKey={newsapi}")
+        if r.status_code == 200:
+            # Parse the JSON response
+            data = r.json()
+            
+            # Extract the articles
+            articles = data.get('articles', [])
+            
+            # Print the headlines
+            for article in articles:
+                speak(article['title'])
+
+    else:
+        # Let OpenAI handle the request
+        output = aiProcess(c)
+        speak(output) 
+
+
+
+
+
+if __name__ == "__main__":
+    speak("Initializing Alena....")
+    while True:
+        # Listen for the wake word "Alena"
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+         
+        print("recognizing...")
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                
+                audio = r.listen(source, timeout=5, phrase_time_limit=3)
+            word = r.recognize_google(audio)
+            if(word.lower() == "alena"):
+                speak("Ya")
+                # Listen for command
+                with sr.Microphone() as source:
+                    print("Alena Active...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+                    processCommand(command)
+
+
+        except Exception as e:
+            print("Error; {0}".format(e))
---
+++ musicLibrary.py
@@ -0,0 +1,9 @@
+music = {
+    "The Knight": "https://www.youtube.com/watch?v=xxYJONmXE8w&list=RDxxYJONmXE8w&start_radio=1&t=219s",
+    
+    "Medieval": "https://youtu.be/KzHkAodaeaI?si=3nQO7OKuWyEGlEhz",
+
+    "thorfinn": "https://youtu.be/7D99fO5E1q8?si=hJEGgzeuxK7CQ71_",
+
+    "wolf": "https://youtu.be/KTmatjyd4KM?si=v-jQTp6eZE6pATkF"
+}
